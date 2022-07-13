a91567b836e66d456eae9f80ce965dbab9e021e2
zhangsan <zhangsan@imooc.com>
2022-07-13 18:07:07 +0800

    add a new file.java

---
+++ GitLearning.java
@@ -0,0 +1,12 @@
+/*
+* 描述：用于学习git
+* */
+public class GitLearning {
+    public static void main(String[] args) {
+        System.out.println("11");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+    }
+}
